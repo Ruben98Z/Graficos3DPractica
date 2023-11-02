b9d7a139b521fc230fe1abd902e2277141a35d83
Ruben98Z <rubenzj98@gmail.com>
2023-11-02 14:43:45 +0100

    Keep aspect ratio and added second object

--- P1GLSL/scr/main.cpp
+++ P1GLSL/scr/main.cpp
@@ -9,6 +9,19 @@
 
 //Idenficadores de los objetos de la escena
 int objId =-1;
+int objId2 = -1;
+// Estado del botton izquierdo del raton
+bool leftPressed = false;
+// Angulo de desplazamiento camara
+float alphaX = 0;
+float alphaY = 0;
+//Matriz identidad
+glm::mat4 I = glm::mat4(1.0);
+// Matriz de proyección
+glm::mat4 proj = glm::mat4(0.);
+// Viewport
+float h = 500;
+float w = 500;
 
 //Declaración de CB
 void resizeFunc(int width, int height);
@@ -26,10 +39,14 @@ int main(int argc, char** argv)
 
 
 	glm::mat4 model = glm::mat4(1.);
-	glm::mat4 proj = glm::mat4(0.);
 	glm::mat4 view = glm::mat4(1.);
 	view[3].z = -6.;
 
+	// Se inicia la matriz de proyeccion
+	// ( 1.73	0		0		0		)
+	// ( 0		1.73	0		0		)
+	// ( 0		0		-1.2	-2.2	)
+	// ( 0		0		-1.0	0		)
 	proj[0].x = 1.73205080;
 	proj[1].y = 1.73205080;
 	proj[2].z = -1.2;
@@ -37,6 +54,7 @@ int main(int argc, char** argv)
 	proj[3].z = -2.2;
    
 	//Se ajusta la cámara
+	// Matriz de Proyección P (Perspectiva)
 	//Si no se da valor se cojen valores por defecto
 	IGlib::setProjMat(proj);
 	IGlib::setViewMat(view);
@@ -44,6 +62,10 @@ int main(int argc, char** argv)
 	//Creamos el objeto que vamos a visualizar
 	objId = IGlib::createObj(cubeNTriangleIndex, cubeNVertex, cubeTriangleIndex, 
 			cubeVertexPos, cubeVertexColor, cubeVertexNormal,cubeVertexTexCoord, cubeVertexTangent);
+
+	//Creamos segundo objeto a visualizar
+	objId2 = IGlib::createObj(cubeNTriangleIndex, cubeNVertex, cubeTriangleIndex,
+		cubeVertexPos, cubeVertexColor, cubeVertexNormal, cubeVertexTexCoord, cubeVertexTangent);
 		
 	glm::mat4 modelMat = glm::mat4(1.0f);
 	IGlib::setModelMat(objId, modelMat);
@@ -67,15 +89,36 @@ int main(int argc, char** argv)
 
 void resizeFunc(int width, int height)
 {
-	//Ajusta el aspect ratio al tamaño de la venta
+	//Ajusta el aspect ratio al tamaño de la ventana
+
+	float aspect = (float)width / (float)height;
+	float right = aspect;
+	float left = -aspect;
+	float top = 1.0;
+	float bottom = -1.0;
+	float near = 1.73;
+
+	proj[0].x = (2 * near) / (right - left); //Apertura horizontal	
+ 
+	std::cout << "Reajuste de la matriz de proyeccion, aspect = " << aspect << std::endl << std::endl;
+
+	IGlib::setProjMat(proj);
+	
 }
 
 void idleFunc()
 {
+	// Primer cubo
 	static float angle = 0.0f;
 	angle += (angle < 2.0f * 3.141592) ? 0.01 : -angle;
-	glm::mat4 model = glm::rotate(glm::mat4(1.0), angle, glm::vec3(1.f, 1.f, 0.f));
+	glm::mat4 model = glm::rotate(I, angle, glm::vec3(1.f, 1.f, 0.f));
 	IGlib::setModelMat(objId, model);
+
+	// Segundo cubo 
+	glm::mat4 rotate90y = glm::rotate(I, angle, glm::vec3(0.f, 1.f, 0.f));
+	glm::mat4 translate = glm::translate(I, glm::vec3(-3.0, 0.0, 0.0));
+	glm::mat4 model2 = rotate90y * translate * rotate90y;
+	IGlib::setModelMat(objId2, model2);
 }
 
 void keyboardFunc(unsigned char key, int x, int y)
@@ -85,10 +128,15 @@ void keyboardFunc(unsigned char key, int x, int y)
 
 void mouseFunc(int button, int state, int x, int y)
 {
-	if (state==0)
+	if (state==0){
 		std::cout << "Se ha pulsado el botón ";
-	else
+		leftPressed = true;
+	}
+	else {
+		leftPressed = false;
 		std::cout << "Se ha soltado el botón ";
+	}
+		
 	
 	if (button == 0) std::cout << "de la izquierda del ratón " << std::endl;
 	if (button == 1) std::cout << "central del ratón " << std::endl;
@@ -99,4 +147,5 @@ void mouseFunc(int button, int state, int x, int y)
 
 void mouseMotionFunc(int x, int y)
 {
+	
 }
